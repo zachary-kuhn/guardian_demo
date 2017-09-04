0244353eb2dc1bf309194ec05bfef783940f54f0
Zachary Kuhn <zachary.kuhn@gmail.com>
2017-09-04 13:42:44 -0400

    Phoenix Router with /users endpoints.

--- lib/guardian_demo_web/router.ex
+++ lib/guardian_demo_web/router.ex
@@ -5,7 +5,34 @@ defmodule GuardianDemoWeb.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :unauthorized do
+    plug :fetch_session
+  end
+
+  pipeline :authorized do
+    plug :fetch_session
+    plug Guardian.Plug.Pipeline, module: GuardianDemo.Guardian,
+      error_handler: GuardianDemo.AuthErrorHandler
+    plug Guardian.Plug.VerifySession
+    plug Guardian.Plug.LoadResource
+  end
+
   scope "/api", GuardianDemoWeb do
     pipe_through :api
+
+    scope "/users" do
+      scope "/" do
+        pipe_through :unauthorized
+  
+        post "/sign-in", UserController, :sign_in
+      end
+  
+      scope "/" do
+        pipe_through :authorized
+  
+        post "/sign-out", UserController, :sign_out
+        get "/me", UserController, :show
+      end
+    end
   end
 end
